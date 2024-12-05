785fc1a671bcec2ec9e4aab6bae21c3c5705a383
Omar Faruk <omarfaruk20@iut-dhaka.edu>
2024-12-05 09:40:01 +0600

    CP TestCase Gen

---
+++ competitive_programming/pycpptest.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env -S uv run
+import json
+import os
+import subprocess
+import sys
+# VS Code runCommand parameters
+# "CreateTestPy": "mkdir -p ${workspaceFolder}/test && cp -n ${homedir}/Project/template_code/competitive_programming/pycpptest.py ${workspaceFolder}/test/pycpptest.py &&  cp ${homedir}/Project/template_code/competitive_programming/test_run.py ${workspaceFolder}/test/test_${fileBasenameNoExtension}.py && chmod +x ${workspaceFolder}/test/test_${fileBasenameNoExtension}.py",
+# "RunCppTest": "${workspaceFolder}/test/test_${fileBasenameNoExtension}.py",
+# 
+# `test_run.py` file is need as template for pytest
+
+def get_problem(filename: str) -> str:
+    ext_len = len(filename.split(".")[-1])+1
+    if filename[:5] == "test_":
+        return filename[5:-ext_len]
+    else:
+        return filename[:-ext_len]
+
+def get_test_cases_cph(problem: str) -> list[tuple[str, str]]:
+    dirname = os.path.dirname(__file__)
+    working_dir = dirname[:-(len("test"))] if dirname[-(len("test")):] == "test" else dirname
+    cph_path = os.path.join(working_dir,".cph")
+    cph_file = None
+    files = os.listdir(cph_path)
+    for file in files:
+        if file.startswith(f".{problem}"):
+            cph_file = os.path.join(cph_path,file)
+            break
+    testcases = []
+    if cph_file is not None:
+        with open(cph_file, 'r') as f:
+            cph = json.load(f)
+            f.close()
+
+        tests = cph["tests"]
+        for test in tests:
+            test_in = test["input"]
+            test_out = test["output"]
+            testcases.append((test_in , test_out))
+
+    return testcases
+
+def compile(problem: str , build_dir: str , compiler: str = "cpp") -> None:
+    # make a build directory if it doesn't exist
+    if not os.path.exists(build_dir):
+        os.makedirs(build_dir)
+    build_problem : str = os.path.join(build_dir,problem)
+    if compiler == "cpp":
+        subprocess.run(["g++","-fsanitize=address","-std=c++20",
+            "-Wall","-Wextra","-Wshadow","-O2",
+            f"./{problem}.cpp" , "-o", f"{build_problem}"])
+    else:
+        print("Error: Compiler is not implemented")
+        sys.exit(1)
+
---
+++ competitive_programming/test_run.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env -S uv run
+import pytest # noqa: F401
+import subprocess
+import os
+import pycpptest
+
+# Get the name of the Python file
+build_dir = "build"
+filename = os.path.basename(__file__)
+dirname = os.path.dirname(__file__)
+problem : str = pycpptest.get_problem(filename)
+build_problem : str = os.path.join(build_dir,problem)
+
+# Extract test cases from cph file
+testcases = pycpptest.get_test_cases_cph(problem)
+
+def run_problem(input: str  , build_problem=f"{build_problem}")->str:
+    return subprocess.check_output([f"./{build_problem}"], input=input.encode('utf-8')).decode('utf-8')
+def print_run_problem(input: str  , build_problem=f"{build_problem}")->str:
+    print(run_problem(input , build_problem))
+
+
+## [ Pytest ] ==================================
+@pytest.mark.parametrize("test_in , test_out" , testcases)
+def test_code(test_in , test_out):
+    assert run_problem(test_in) == test_in
+
+def test_gen():
+    assert True
+
+
+
+## [ Main ] (run from Script) ==================
+if __name__ == "__main__":
+    pycpptest.compile(problem , build_dir)
+    filepath = os.path.join(dirname,filename)
+    os.system(f"pytest -vv {filepath}")
