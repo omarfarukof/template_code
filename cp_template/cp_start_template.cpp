1df9d5f564a58fe28c8623c679e42b778ac162eb
Omar Faruk <omarfaruk20@iut-dhaka.edu>
2024-12-25 17:37:34 +0600

    CP_Templates

---
+++ cp_template/cp_start_template.cpp
@@ -0,0 +1,24 @@
+#include "bits/stdc++.h"
+using namespace std;
+
+
+void SolveCode(){
+    // Solve Code
+
+
+}
+
+
+int main(){
+    int t;
+    cin >> t;
+
+    for (int TEST_CASE_NO=0; TEST_CASE_NO<t; TEST_CASE_NO++){
+        //Solve Code
+        SolveCode();
+
+    }
+
+
+    return 0;
+}
